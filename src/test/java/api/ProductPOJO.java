cafc5dc78e7f01bcad6594db06a8968a080fbd68
inalamadhu <madhuinala0109@gmail.com>
2023-05-21 09:42:56 +0530

    updated test methods

---
+++ src/test/java/api/ProductPOJO.java
@@ -0,0 +1,96 @@
+package api;
+
+public class ProductPOJO {
+    public String name;
+    public String type;
+
+    public Float price;
+    public String upc;
+    public Integer shipping;
+    public String description;
+    public String manufacturer;
+    public String model;
+    public String url;
+    public String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getUpc() {
+        return upc;
+    }
+
+    public void setUpc(String upc) {
+        this.upc = upc;
+    }
+
+    public Integer getShipping() {
+        return shipping;
+    }
+
+    public void setShipping(Integer shipping) {
+        this.shipping = shipping;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
--- src/test/java/api/Products.java
+++ src/test/java/api/Products.java
@@ -1,23 +1,35 @@
 package api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
 import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.xml.XmlPath;
 import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.*;
 
 public class Products {
 
+    RequestSpecBuilder specBuilder;
+    RequestSpecification requestSpecification;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void beforeMethod(){
         RestAssured.baseURI = "http://localhost:3030";
     }
@@ -30,9 +42,14 @@ public class Products {
                     .get("/products")
                 .then().extract();
 
-               Map<String, ?> list = extRes.path("data.find{it.manufacturer == 'Duracell' && it.model=='MN2400B4Z'}");
+               String str =extRes.path("data.find{it.manufacturer == 'Duracell' && it.model=='MN2400B4Z'}.categories.find{it.name=='Household Batteries'}.name");
                // extRes.path("data.find{it.manufacturer == 'Duracell' && it.model=='MN2400B4Z'}.categories.find{it.name=='Household Batteries'}")
-        System.out.println(list);
+
+                Assert.assertEquals(str, "Household Batteries");
+//            List<Map<String,?>> list = extRes.path("data.findAll{it.type=='Test2_HardGood'}", );
+
+
+        System.out.println(str);
 
 
 
@@ -52,17 +69,39 @@ public class Products {
 
     }
 
-    @Test(enabled=false, groups="jsonfilter")
+    @Test(enabled=true, groups="getproduct")
     public void getProducts3(){
         RequestSpecBuilder builder = new RequestSpecBuilder();
         builder.addHeader("","");
         RequestSpecification spec = builder.build();
 
-                given()
+                ValidatableResponse res = given()
                       .header("content-type", "application/json")
                 .when()
                 .get("/products")
-                .then().body("data.find{it.manufacturer == 'Duracell' && it.model=='MN2400B4Z'}", hasKey("shipping1"));
+                .then();
+
+                res.body("data.find{it.manufacturer == 'Duracell' && it.model=='MN2400B4Z'}", hasKey("shipping"));
+
+
+    }
+
+    @Test(groups="postproduct")
+    public void postProducts() throws FileNotFoundException {
+
+        Gson gson = new GsonBuilder().create();
+        JsonReader reader = new JsonReader(new FileReader(System.getProperty("user.dir")+"/src/resources/product.json"));
+        ProductPOJO product = gson.fromJson(reader, ProductPOJO.class);
+
+        System.out.println(product.toString());
+        ValidatableResponse ext = given()
+                .header("content-type", "application/json")
+                .when()
+                    .body(product)
+                    .post("/products")
+                .then();
+
+        ext.body(".", hasValue("Test2_Duracell - AAA Batteries (4-Pack)"));
 
 
 
